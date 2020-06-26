f01752d5c01b22bc3b066acdab2605c45d921161
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-26 21:05:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     "#{season.to_s.capitalize!}:"
     data.each do |holiday, supply_list|
-      
+      array = holiday.split.to_s
       
   binding.pry
 
