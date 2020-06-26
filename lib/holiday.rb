d3ca6236695d47370119b7596bbd45ae1f3d0afa
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-26 20:37:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -7,6 +7,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :christmas => ["Lights", "Wreath"],
   #     :new_years => ["Party Hats"]
   #   },
+  
   #   :summer => {
   #     :fourth_of_july => ["Fireworks", "BBQ"]
   #   },
