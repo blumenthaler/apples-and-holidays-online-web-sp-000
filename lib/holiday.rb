cef486ff7fa2b839fcaadb0cda953ffbdd946a13
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-26 20:41:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,6 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash[:winter].each do |winter_holiday|
     winter_holiday = winter_holiday << supply
+    binding.pry
   end
 end
 
